d3dd7637af9f44fbe5ffcfeaf14a09d0049a5cb3
Oleg Tielushko <tielushko@mail.usf.edu>
2020-10-16 21:06:10 -0400

    Birthday reminder completed

--- 1-birthday-reminder/setup/src/List.js
+++ 1-birthday-reminder/setup/src/List.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React from "react";
 
-const List = () => {
+
+const List = ({people}) => {
   return (
-    <>
-      <h2>list component</h2>
-    </>
+    <React.Fragment>
+      {people.map((person) => {
+        const { id, name, age, image } = person;
+        return (
+          <article className="person" id={id}>
+            <img src={image} alt=""></img>
+            <div>
+              <h4>{name}</h4>
+              <p>{age} years</p>
+            </div>
+          </article>
+        );
+      })}
+    </React.Fragment>
   );
 };
 
